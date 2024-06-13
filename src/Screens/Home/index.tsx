205aa39f93883bfc59e14ca716f1b0bb57223434
laiscrz <lais.albino2003@gmail.com>
2024-06-13 16:18:01 -0300

    feat: create structure home screen

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,11 +1,17 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export function Home() {
     return (
-        <LinearGradient colors={["#292A4E", "#715C77", "#C75C2E"]} style={styles.container}>
-            
+        <LinearGradient colors={["#292A4E", "#715C77", "#C75C2E"]}
+            style={styles.container}>
+            <View style={styles.content}>
+                <View style={styles.header}>
+                    <View style={styles.headerLeft}>
+                        <Text>São Paulo</Text>
+                    </View>
+                </View>
+            </View>
         </LinearGradient>
     );
 }
@@ -13,6 +19,15 @@ export function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        
+
+    },
+    content : {
+
+    },
+    header : {
+
+    },
+    headerLeft : {
+
     }
 })
