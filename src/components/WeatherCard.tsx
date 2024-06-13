d7c5768c1814bbf90fa0b43e4b2552c50519cdbe
laiscrz <lais.albino2003@gmail.com>
2024-06-13 18:35:01 -0300

    feat: create 'WeatherCard.tsx'

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Sun02d from '../../assets/image/02d.svg';
+import { styles } from '../styles/index';
+import { WeatherCardProps } from '../@types/weatherTypes'; // Importe a interface WeatherCardProps
+
+const WeatherCard: React.FC<WeatherCardProps> = ({ dayOfWeek, temperature }) => {
+    return (
+        <View style={styles.infoDetailsCard}>
+            <Text style={styles.infoDetailsCardWeekDay}>{dayOfWeek}</Text>
+            <Sun02d width={40} height={40} />
+            <Text style={styles.infoDetailsCardPreview}>{temperature}°C</Text>
+        </View>
+    );
+}
+
+export default WeatherCard;
+
